29bf2715bc9c9bdad02cb705428b81a8b7f99e80
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2012-12-26 14:32:04 -0500

    CherryPy example works now

--- experimental/cherrypy/test01.py
+++ experimental/cherrypy/test01.py
@@ -1,16 +1,33 @@
 import cherrypy
+import simplejson
+import os
+
 from mako.template import Template
 from mako.lookup import TemplateLookup
 lookup = TemplateLookup(directories=['html'])
 
-class Root:
+JS_DIR = os.path.join(os.path.abspath("."), u"js")
+
+class Root(object):
     @cherrypy.expose
     def index(self):
         index_html = os.path.join(os.path.dirname(__file__), 'index.html')
         tmpl = lookup.get_template(index_html)
         return tmpl.render(name="aashish")
 
-cherrypy.tree.mount(Root())
+    @cherrypy.expose
+    def update(self):
+      # Here's the important message!
+      name = "My name is aashish"
+      return name
+
+config = {'/js':
+                {'tools.staticdir.on': True,
+                 'tools.staticdir.dir': JS_DIR,
+                }
+        }
+
+cherrypy.tree.mount(Root(), '/', config=config)
 
 if __name__ == '__main__':
     import os.path
