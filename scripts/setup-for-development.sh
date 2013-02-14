c66be15d0d604f115feeb09da051ef7cb037f7ac
Brad King <brad.king@kitware.com>
2013-02-14 14:21:18 -0500

    Configure general GitSetup scripts for 'opengeoscience'
    Add scripts/gitsetup/config to configure the generalized scripts for this
    project.  Add scripts/setup-for-development.sh to run the generalized scripts.
    Tell Git in .gitattributes not to export gitsetup.

---
+++ scripts/setup-for-development.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd "${BASH_SOURCE%/*}/.." &&
+scripts/gitsetup/setup-user && echo &&
+scripts/gitsetup/setup-hooks && echo &&
+(scripts/gitsetup/setup-ssh ||
+ echo 'Failed to setup SSH.  Run this again to retry.') && echo &&
+scripts/gitsetup/tips
+
+# Rebase master by default
+git config rebase.stat true
+git config branch.master.rebase true
+
+echo 'Configuring push url...' &&
+git config remote.origin.pushurl git@public.kitware.com:OpenGeoscience/opengeoscience
