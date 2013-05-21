12ae69db0141020c97fe91f736deca0b7699d805
Dave DeMarle <dave.demarle@kitware.com>
2013-05-21 11:43:26 -0400

    optimize geojson import

--- modules/vtk_reader.py
+++ modules/vtk_reader.py
@@ -15,17 +15,23 @@ def read(expr, vars):
   ''' Read a file or files from a directory given a wild-card expression
   '''
   # @todo Reading a single file of netcdf cf convention now
+
+  #tlog = vtk.vtkTimerLog()
+  #t0 = tlog.GetUniversalTime()
   ss = vtk.vtkNetCDFCFReader() #get test data
   ss.SphericalCoordinatesOff()
   ss.SetOutputTypeToImage()
   datadir = cherrypy.request.app.config['/data']['tools.staticdir.dir']
   filename = os.path.join(datadir, expr)
   ss.SetFileName(filename)
+  #ss.Update()
+  #t1 = tlog.GetUniversalTime()
 
   # Convert to polydata
   sf = vtk.vtkDataSetSurfaceFilter()
   sf.SetInputConnection(ss.GetOutputPort())
-  sf.Update()
+  #sf.Update()
+  #t2 = tlog.GetUniversalTime()
 
   # Convert to GeoJSON
   gw = vtk.vtkGeoJSONWriter()
@@ -34,4 +40,7 @@ def read(expr, vars):
   gw.SetScalarFormat(2)
   gw.Write()
   gj = str(gw.RegisterAndGetOutputString()).replace('\n','')
+  #t3 = tlog.GetUniversalTime()
+
+  #cherrypy.log("VTKTIME: " + str(t1-t0) + " + " + str(t2-t1) + " + " + str(t3-t2))
   return gj
