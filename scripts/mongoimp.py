2ef01f858f011b4d6fa68a0c8d5109f8c745de8b
Chris Harris <chris.harris@kitware.com>
2013-07-23 13:06:23 -0400

    Move scripts/standardtime.py -> modules/standardtime.py
    The module needs to be in the same directory for running
    from the source tree.

--- scripts/mongoimp.py
+++ scripts/mongoimp.py
@@ -2,6 +2,12 @@
 
 import pymongo
 import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+modules_dir = os.path.join(current_dir, "..", "modules")
+sys.path.append(modules_dir)
+
 from standardtime import attrib_to_converters
 
 class mongo_import:
