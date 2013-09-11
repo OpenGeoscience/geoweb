d027d1f2844d1dc78f98ee367d78833368f1bca8
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-09-11 14:13:00 -0400

    Added missing file

---
+++ cmake/configure.cmake
@@ -0,0 +1,31 @@
+set(STATICDIR_ROOT)
+set(SRC_DIR)
+set(BIN_DIR)
+set(DEPLOY_DIR)
+set(DATA_ROOT)
+set(LOG_ERROR_FILE)
+set(LOG_ACCESS_FILE)
+set(GEOWEB_USER)
+set(GEOWEB_GROUP)
+set(VTK_ROOT)
+set(VisTrails_ROOT)
+set(GEOWEB_CMAKE_DIR)
+set(GEOWEB_DEPLOY_DIR)
+
+message("hey.....")
+
+configure_file(${GEOWEB_CMAKE_DIR}/setup-runtime.sh.in
+  ${CMAKE_CURRENT_BINARY_DIR}/${GEOWEB_DEPLOY_DIR}/setup-runtime.sh @ONLY
+)
+
+configure_file(${GEOWEB_CMAKE_DIR}/configure_server.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/configure_server.cmake @ONLY
+)
+
+configure_file(${GEOWEB_CMAKE_DIR}/geocelery_conf.py.in
+  ${CMAKE_CURRENT_BINARY_DIR}/${GEOWEB_DEPLOY_DIR}/modules/geocelery_conf.py @ONLY
+)
+
+configure_file(${GEOWEB_CMAKE_DIR}/run.sh.in
+  ${CMAKE_CURRENT_BINARY_DIR}/${GEOWEB_DEPLOY_DIR}/run.sh @ONLY
+)
