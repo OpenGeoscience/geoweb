c8ae04dc3f9648085996940f26d5d44aba19e63d
Chris Harris <chris.harris@kitware.com>
2014-03-05 14:38:00 -0500

    WIP: Working

--- web/apps/floodmap/archive.js
+++ web/apps/floodmap/archive.js
@@ -13,6 +13,8 @@ archive.queryCachedLon = null;
 archive.queryCachedVarname = null;
 archive.timeseriesPlot = null;
 
+archive.milliseconds_per_year = 1000 * 60 * 60 * 24 * 365.25;
+
 //////////////////////////////////////////////////////////////////////////////
 /**
  * Display error message
@@ -89,7 +91,7 @@ archive.getMongoConfig = function() {
 //////////////////////////////////////////////////////////////////////////////
 archive.initQueryInterface = function() {
 
-  $('#document-table-body').tooltip();
+  $('#results-list').tooltip();
 
   $('#dateFrom').datepicker();
   $('#dateTo').datepicker();
@@ -109,7 +111,7 @@ archive.initQueryInterface = function() {
     var query = $('#query-input').val();
     if (query.length == 0) {
       $('#query-input').removeClass("query-in-progress");
-      $('#document-table-body').empty();
+      $('#results-list').empty();
       // Stop the processing of any pending queries
       archive.lastDatabaseQueryProcessed = archive.databaseQueryId++
       archive.lastEsgfQueryProcessed = archive.esgfQueryId++
@@ -130,13 +132,14 @@ archive.initQueryInterface = function() {
       var target = $(ui.helper).data("dataset");
       if (target) {
         // The user now knows how to add layers to the map so remove tool tip
-        $('#document-table-body').tooltip('disable')
+        $('#results-list').tooltip('disable');
         archive.addLayer(target);
       }
     }
   });
 
   $('#drawRegion').off('click').click(function() {
+    console.log("click");
     var active = $(this).toggleClass('active').hasClass('active');
     archive.myMap.viewer().interactorStyle().drawRegionMode(active);
     if(active &&
@@ -197,128 +200,209 @@ archive.processESGFResults = function(results, remove) {
 //////////////////////////////////////////////////////////////////////////////
 archive.processResults = function(results, removeFilter) {
 
-  var tr = d3.select('#document-table-body').selectAll("tr")
-    .data(results, function(d) {
-      return d['id'];
-    });
-
-  var rows = tr.enter().append('tr');
-
-  $.each(tr.exit()[0], function(index, row) {
-    if (row) {
-      selection = d3.select(row);
-      if (removeFilter(selection.data()[0]))
-        selection.remove();
+  var toRemove = [];
+  $('#results-list div').each(function(index){
+    var dset = $(this).data('dataset');
+    if(dset && removeFilter(dset)) {
+      toRemove.push($(this));
     }
   });
 
-  var td = rows.selectAll('td')
-    .data(function(row) {
-      // Display the tags, we should probably truncate the list ...
-      var tags = []
-      $.each(row['variables'], function(index, variable) {
-        tags = tags.concat(variable['tags']);
-      });
+  $.each(toRemove, function(index, value) {
+    value.remove();
+  });
 
-      var size = row['size'];
+  function createResultListItem(dataset, variable, size, source, timeRange) {
 
-      if (size && size != 'N/A') {
-        size = Math.round(row['size']/1024/1024) + "M"
+    function getVariableTagsOrName() {
+      if('tags' in variable && variable['tags'].length > 0) {
+        return variable['tags'].slice(0,3).join(',');
+      } else if('long_name' in variable) {
+        return variable['long_name'];
+      } else {
+        var long_name_map = {
+          'clt': 'Total Cloudiness',
+          'tas': 'Surface Air Temperature'
+        };
+        if(variable['name'] in long_name_map) {
+          return long_name_map[variable['name']];
+        }
       }
+      return variable['name'];
+    }
 
-      return [ {column: 'name', data: row['name']},
-               {column: 'source', data: row['source']},
-               {column: 'size', data: size},
-               {column: 'tags', data: tags.join()}] ;
+    var $li = $([
+      '<div class="variable-item"><div style="pointer-events: none;">',
+      '<i class="icon-th pull-left"></i> ',
+      '<div class="variable-name">',
+      getVariableTagsOrName(),
+      '</div>',
+      timeRange,
+      source,
+      size,
+      '</div></div>'
+    ].join(''));
+    $li.data('dataset', dataset);
+    $li.data('variable', variable);
+    return $li;
+  }
+
+  $.each(results, function(index, dataset) {
+    var size = dataset['size'],
+      source = ['<span class="badge badge-success pull-right">', dataset['source'], '</span>'].join(''),
+      timeRange = '&nbsp;',//some text is needed, else the layout may get messed up
+      start,
+      end;
+
+    if (size && size != 'N/A') {
+      size = [
+        '<span class="badge pull-right">',
+        Math.round(dataset['size']/1024/1024),
+        'M</span>'
+      ].join('');
+    } else {
+      size = '';
+    }
+
+    if(dataset && 'timeInfo' in dataset && 'dateRange' in dataset['timeInfo']) {
+      start = dataset['timeInfo'].dateRange[0];
+      end =  dataset['timeInfo'].dateRange[1];
+      start = new Date(start[0], start[1], start[2], 0, 0, 0, 0);
+      end = new Date(end[0], end[1], end[2], 0, 0, 0, 0);
+      timeRange = [
+        [
+          start.getMonth()+1,
+          start.getDate(),
+          start.getYear()
+        ].join('/'),
+        [
+          end.getMonth()+1,
+          end.getDate(),
+          end.getYear()
+        ].join('/')
+      ].join(' - ')
+    }
+
+    $.each(dataset['variables'], function(_index, variable) {
+      $('#results-list').append(createResultListItem(dataset, variable, size, source,
+        timeRange));
     });
-
-    td = td.enter().append('td');
-    td.text(function(d) { return d['data']; });
-    td.each(function(d, i) { $(this).addClass(d['column']);});
-
-  // Populate the timesteps parameter list
-  var selectTimestep = rows.append('td');
-  selectTimestep.classed('timesteps', true);
-  selectTimestep = selectTimestep.append('select')
-  selectTimestep.classed('timestep-select', true);
-
-  selectTimestep = selectTimestep.selectAll('select').data(function(row) {
-
-    var timestep  = ['N/A'];
-
-    if (row && 'timeInfo' in row && row['timeInfo'].rawTimes)
-      timestep = row['timeInfo'].rawTimes;
-
-    return timestep;
   });
 
-  selectTimestep.enter().append('option').text(function(timestep) {
-    return timestep;
-  })
-  selectTimestep.exit().remove();
+//  var tr = d3.select('#results-list').selectAll("li")
+//    .data(results, function(d) {
+//      return d['id'];
+//    });
+//
+//  var rows = tr.enter().append('li');
+//
+//  $.each(tr.exit()[0], function(index, row) {
+//    if (row) {
+//      selection = d3.select(row);
+//      if (removeFilter(selection.data()[0]))
+//        selection.remove();
+//    }
+//  });
+//
+//  var content = rows.selectAll('div')
+//    .data(function(row) {
+//      // Display the tags, we should probably truncate the list ...
+//      var tags = [];
+//      $.each(row['variables'], function(index, variable) {
+//        tags = tags.concat(variable['tags']);
+//      });
+//
+//      var size = row['size'];
+//
+//      if (size && size != 'N/A') {
+//        size = Math.round(row['size']/1024/1024) + "M"
+//      }
+//
+//      return [ {column: 'name', data: row['name']},
+//               {column: 'source', data: row['source']},
+//               {column: 'size', data: size},
+//               {column: 'tags', data: tags.join()}] ;
+//    });
+//
+//  content = content.enter().append('div');
+//  content.text(function(d) { return d['data']; });
+//  content.each(function(d, i) { $(this).addClass(d['column']);});
+//
+//  // Populate the timesteps parameter list
+//  var selectTimestep = rows.append('td');
+//  selectTimestep.classed('timesteps', true);
+//  selectTimestep = selectTimestep.append('select');
+//  selectTimestep.classed('timestep-select', true);
+//
+//  selectTimestep = selectTimestep.selectAll('select').data(function(row) {
+//
+//    var timestep  = ['N/A'];
+//
+//    if (row && 'timeInfo' in row && row['timeInfo'].rawTimes)
+//      timestep = row['timeInfo'].rawTimes;
+//
+//    return timestep;
+//  });
+//
+//  selectTimestep.enter().append('option').text(function(timestep) {
+//    return timestep;
+//  });
+//  selectTimestep.exit().remove();
+//
+//  // Populate the parameter list
+//  var select = rows.append('td');
+//  select.classed('parameter', true);
+//  select = select.append('select');
+//  select.classed("parameter-select", true);
+//
+//  select = select.selectAll('select').data(function(row) {
+//    var variables = [];
+//    $.each(row['variables'], function(index, variable) {
+//      variables = variables.concat(variable['name']);
+//    });
+//
+//    return variables;
+//  });
+//
+//  select.enter().append('option').text(function(variable) {
+//    return variable;
+//  });
+//  select.exit().remove();
 
-  // Populate the parameter list
-  var select = rows.append('td');
-  select.classed('parameter', true);
-  select = select.append('select');
-  select.classed("parameter-select", true);
-
-  select = select.selectAll('select').data(function(row) {
-    var variables = [];
-    $.each(row['variables'], function(index, variable) {
-      variables = variables.concat(variable['name']);
-    });
-
-    return variables;
-  });
-
-  select.enter().append('option').text(function(variable) {
-    return variable;
-  });
-  select.exit().remove();
-
-  $('#document-table  tr').draggable( {
+  $('#results-list div').draggable( {
     cursor: 'move',
     containment: 'window',
     appendTo: 'body',
     helper: function(event) {
+      var $this = $(this),
+        dataset = $this.data('dataset'),
+        variable = $this.data('variable'),
+        timestep = null,
+        drag;
 
-    var parameter = $('.parameter-select', this).val();
-    var timestep = $('.timestep-select', this).val();
+    if (dataset && 'timeInfo' in dataset && 'rawTimes' in dataset['timeInfo']) {
+      timestep = dataset['timeInfo'].rawTimes[0];
+    }
 
-    var timesteps = [];
-    $('.timestep-select option', this).each(function() {
-      timesteps.push(parseInt($(this).val()));
-    });
-
-    if (timestep == 'N/A')
-      timestep = null;
-
-    var data = d3.select(this).data();
-
-    drag = $('<div id="parameter" class="whatadrag">' + parameter + '</div>');
+    drag = $('<div id="parameter" class="whatadrag">' + variable['name'] + '</div>');
 
     drag.data("dataset", {
-      name: data[0].name,
-      dataset_id: data[0].id,
-      source: data[0].source,
-      parameter: parameter,
+      name: dataset.name,
+      dataset_id: dataset.id,
+      source: dataset.source,
+      parameter: variable['name'],
       timestep: timestep,
-      timeInfo: data[0].timeInfo,
-      url: data[0].url,
-      size: data[0].size,
-      checksum: data[0].checksum,
-      basename: data[0].basename
+      timeInfo: dataset.timeInfo,
+      url: dataset.url,
+      size: dataset.size,
+      checksum: dataset.checksum,
+      basename: dataset.basename
     });
 
     return drag;
     }
   })
 };
-
-archive.databaseQueryId = 0;
-archive.lastDatabaseQueryProcessed = -1;
-archive.milliseconds_per_year = 1000 * 60 * 60 * 24 * 365.25; //not "exact", I know, give me a break...
 
 //////////////////////////////////////////////////////////////////////////////
 /**
@@ -410,7 +494,6 @@ archive.queryDatabase = function(query) {
   addQueryRestraint('spatialInfo.1', '$gte', '#longitudeFrom', parseFloat);
   addQueryRestraint('spatialInfo.2', '$lte', '#latitudeTo', parseFloat);
   addQueryRestraint('spatialInfo.3', '$gte', '#latitudeFrom', parseFloat);
-
 
   $(archive).trigger('query-started');
 
@@ -549,13 +632,41 @@ archive.queryESGF = function(query) {
 
   $(archive).trigger('query-started');
 
+  var start, end, bbox;
+
+  if ($('#dateFrom').val()) {
+    // We add the time zone postfix so we get Date object in the correct time
+    // zone.
+    start = new Date($('#dateFrom').val() + ' 00:00:00 GMT').toISOString()
+  }
+
+
+  if ($('#dateTo').val()) {
+    // We add the time zone postfix so we get Date object in the correct time
+    // zone.
+    end = new Date($('#dateTo').val() + ' 00:00:00 GMT').toISOString()
+  }
+
+
+  // If one of lat/long fields have been filled in we know a bounding box has
+  // been selected so include it in our query. The bounding box needs to be of
+  // the following form: [west, south, east, north]
+  if ($('#longitudeFrom').val()) {
+    bbox = [$('#longitudeFrom').val(), $('#latitudeFrom').val(),
+            $('#longitudeTo').val(), $('#latitudeTo').val()]
+  }
+
+  data =  { queryId: archive.esgfQueryId++,
+            expr: JSON.stringify(query),
+            'start': start,
+            'end': end,
+            bbox: JSON.stringify(bbox)
+          }
+
   $.ajax({
     type: 'POST',
     url: '/services/esgf/query',
-    data: {
-      queryId: archive.esgfQueryId++,
-      expr: JSON.stringify(query)
-    },
+    data: data,
     dataType: 'json',
     success: function(response) {
       if (response.error !== null) {
@@ -605,6 +716,28 @@ archive.main = function() {
   $(function() {
     archive.myMap = ogs.geo.map(document.getElementById("glcanvas"), mapOptions);
     var canvas = document.getElementById('glcanvas');
+
+    $(archive.myMap.viewer()).on(vglModule.command.mouseReleaseEvent, function(event) {
+
+      if ($('#drawRegion').hasClass('active')) {
+        var latFrom = parseFloat($('#latitudeFrom').val());
+        var latTo = parseFloat($('#latitudeTo').val());
+        var longFrom = parseFloat($('#longitudeFrom').val());
+        var longTo = parseFloat($('#longitudeTo').val());
+
+        var bbox = [[longFrom, latFrom], [longFrom, latTo], [longTo, latTo], [longTo, latFrom], [longFrom, latFrom]]
+
+        console.log("bbox: " + bbox);
+
+        archive.addLayerToMap(bbox);
+
+        archive.myMap.viewer().interactorStyle().drawRegionMode(false);
+        $('#drawRegion').toggleClass('active')
+
+        //$('#drawRegion').toggleClass('active')
+        console.log("mouse released");
+      }
+    });
 
     // Resize the canvas to fill browser window dynamically
     window.addEventListener('resize', resizeCanvas, false);
@@ -811,10 +944,10 @@ archive.main = function() {
       function() {
         var coords = archive.myMap.getInteractorStyle().getDrawRegion();
 
-        $('#latitudeFrom').val(coords[0]);
-        $('#longitudeFrom').val(coords[1]);
-        $('#latitudeTo').val(coords[2]);
-        $('#longitudeTo').val(coords[3]);
+        $('#latitudeFrom').val(coords[0] < coords[2] ? coords[0] : coords[2]);
+        $('#longitudeFrom').val(coords[1] < coords[3] ? coords[1] : coords[3]);
+        $('#latitudeTo').val(coords[0] > coords[2] ? coords[0] : coords[2] );
+        $('#longitudeTo').val(coords[1] > coords[3] ? coords[1] : coords[3]);
       }
     );
 
@@ -864,7 +997,7 @@ archive.main = function() {
     archive.logOut();
   });
 
-  archive.addLayerToMap();
+  //archive.addLayerToMap();
 };
 
 //////////////////////////////////////////////////////////////////////////////
@@ -1336,19 +1469,16 @@ archive.downloadESGF = function(target, onComplete, message) {
  * @param algorithm
  */
 //////////////////////////////////////////////////////////////////////////////
-archive.addLayerToMap = function() {
+archive.addLayerToMap = function(bbox) {
 
-  layer = ogs.geo.featureLayer();
-  floodSource = ogs.geo.floodLayerSource();
+  var layer = ogs.geo.featureLayer(),
+      floodSource = ogs.geo.floodLayerSource(bbox);
+
   layer.setDataSource(floodSource);
   layer.update(ogs.geo.updateRequest(1));
 
   archive.myMap.addLayer(layer);
   archive.myMap.draw();
-
-  if(archive.tutorialMask.isOff('layerOptions')) {
-    $('#layers').tooltip('show');
-  }
 };
 
 
