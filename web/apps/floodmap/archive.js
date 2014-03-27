b36aee3476d3c362a21e5c6f737012d0d4623f3e
Chris Harris <chris.harris@kitware.com>
2014-03-27 11:35:32 -0400

    More clean up

--- web/apps/floodmap/archive.js
+++ web/apps/floodmap/archive.js
@@ -3,18 +3,7 @@
 
 var archive = {};
 archive.myMap = null;
-archive.queriesInProgress = 0;
-archive.databaseQueryId = 0;
-archive.lastDatabaseQueryProcessed = -1;
-archive.esgfQueryId = 0;
-archive.lastEsgfQueryProcessed = -1;
-archive.queryCachedLat = null;
-archive.queryCachedLon = null;
-archive.queryCachedVarname = null;
-archive.timeseriesPlot = null;
 archive.floodLayer = null
-
-archive.milliseconds_per_year = 1000 * 60 * 60 * 24 * 365.25;
 
 //////////////////////////////////////////////////////////////////////////////
 /**
@@ -34,73 +23,14 @@ archive.error = function(errorString, onClose) {
 
 //////////////////////////////////////////////////////////////////////////////
 /**
- * Invalid query cache
+ * Main program
  *
- * @param callback
  */
 //////////////////////////////////////////////////////////////////////////////
-archive.resetQueryCache = function() {
-  archive.queryCachedLat = null;
-  archive.queryCachedLon = null;
-  archive.queryCachedVarname = null;
-  archive.timeseriesPlot = null;
-};
+archive.main = function() {
+  init();
 
-
-//////////////////////////////////////////////////////////////////////////////
-/**
- * Setup the basic query components and bindings.
- */
-//////////////////////////////////////////////////////////////////////////////
-archive.initQueryInterface = function() {
-
-  $('#results-list').tooltip();
-
-  $('#dateFrom').datepicker();
-  $('#dateTo').datepicker();
-
-  $(archive).on('query-started', function() {
-    archive.queriesInProgress++;
-    $('#query-input').addClass("query-in-progress");
-  });
-
-  $(archive).on('query-canceled query-finished query-error', function() {
-    archive.queriesInProgress--;
-    if (archive.queriesInProgress == 0)
-      $('#query-input').removeClass("query-in-progress");
-  });
-
-  $('#query-input').bind("keyup", function() {
-    var query = $('#query-input').val();
-    if (query.length == 0) {
-      $('#query-input').removeClass("query-in-progress");
-      $('#results-list').empty();
-      // Stop the processing of any pending queries
-      archive.lastDatabaseQueryProcessed = archive.databaseQueryId++
-      archive.lastEsgfQueryProcessed = archive.esgfQueryId++
-    }
-    else {
-      archive.queryDatabase(query);
-      archive.queryESGF(query);
-
-      if(archive.tutorialMask.isOff('dragAndDrop')) {
-        $('#document-table-body').tooltip('show');
-        archive.tutorialMask.turnOn('dragAndDrop');
-      }
-    }
-  });
-
-  $('#glcanvas').droppable({
-    drop: function(event, ui) {
-      var target = $(ui.helper).data("dataset");
-      if (target) {
-        // The user now knows how to add layers to the map so remove tool tip
-        $('#results-list').tooltip('disable');
-        archive.addLayer(target);
-      }
-    }
-  });
-
+  $('#error-dialog').hide();
   $('#draw-bbox').off('click').click(function() {
     console.log("click");
     var active = $(this).toggleClass('active').hasClass('active');
@@ -119,265 +49,7 @@ archive.initQueryInterface = function() {
       );
     }
   });
-};
 
-//////////////////////////////////////////////////////////////////////////////
-/**
- * Process the result coming from mongo.
- */
-//////////////////////////////////////////////////////////////////////////////
-archive.processLocalResults = function(results, remove) {
-  remove = typeof remove !== 'undefined' ? remove : false;
-
-  var removeFilter = function(d) {return false};
-  if (remove) {
-    removeFilter = function(d) {return d['source'] == 'Local'};
-  }
-
-  archive.processResults(results, removeFilter);
-};
-
-//////////////////////////////////////////////////////////////////////////////
-/**
- * Process ESGF results
- *
- * @param results
- * @param remove
- */
-//////////////////////////////////////////////////////////////////////////////
-archive.processESGFResults = function(results, remove) {
-  remove = typeof remove !== 'undefined' ? remove : false;
-
-  removeFilter = function(d) {return false};
-  if (remove) {
-    removeFilter = function(d) {return d['source'] == 'ESGF'};
-  }
-
-  archive.processResults(results, removeFilter);
-}
-
-//////////////////////////////////////////////////////////////////////////////
-/**
- * Use D3 to update document table with results.
- */
-//////////////////////////////////////////////////////////////////////////////
-archive.processResults = function(results, removeFilter) {
-
-  var toRemove = [];
-  $('#results-list div').each(function(index){
-    var dset = $(this).data('dataset');
-    if(dset && removeFilter(dset)) {
-      toRemove.push($(this));
-    }
-  });
-
-  $.each(toRemove, function(index, value) {
-    value.remove();
-  });
-
-  function createResultListItem(dataset, variable, size, source, timeRange) {
-
-    function getVariableTagsOrName() {
-      if('tags' in variable && variable['tags'].length > 0) {
-        return variable['tags'].slice(0,3).join(',');
-      } else if('long_name' in variable) {
-        return variable['long_name'];
-      } else {
-        var long_name_map = {
-          'clt': 'Total Cloudiness',
-          'tas': 'Surface Air Temperature'
-        };
-        if(variable['name'] in long_name_map) {
-          return long_name_map[variable['name']];
-        }
-      }
-      return variable['name'];
-    }
-
-    var $li = $([
-      '<div class="variable-item"><div style="pointer-events: none;">',
-      '<i class="icon-th pull-left"></i> ',
-      '<div class="variable-name">',
-      getVariableTagsOrName(),
-      '</div>',
-      timeRange,
-      source,
-      size,
-      '</div></div>'
-    ].join(''));
-    $li.data('dataset', dataset);
-    $li.data('variable', variable);
-    return $li;
-  }
-
-  $.each(results, function(index, dataset) {
-    var size = dataset['size'],
-      source = ['<span class="badge badge-success pull-right">', dataset['source'], '</span>'].join(''),
-      timeRange = '&nbsp;',//some text is needed, else the layout may get messed up
-      start,
-      end;
-
-    if (size && size != 'N/A') {
-      size = [
-        '<span class="badge pull-right">',
-        Math.round(dataset['size']/1024/1024),
-        'M</span>'
-      ].join('');
-    } else {
-      size = '';
-    }
-
-    if(dataset && 'timeInfo' in dataset && 'dateRange' in dataset['timeInfo']) {
-      start = dataset['timeInfo'].dateRange[0];
-      end =  dataset['timeInfo'].dateRange[1];
-      start = new Date(start[0], start[1], start[2], 0, 0, 0, 0);
-      end = new Date(end[0], end[1], end[2], 0, 0, 0, 0);
-      timeRange = [
-        [
-          start.getMonth()+1,
-          start.getDate(),
-          start.getYear()
-        ].join('/'),
-        [
-          end.getMonth()+1,
-          end.getDate(),
-          end.getYear()
-        ].join('/')
-      ].join(' - ')
-    }
-
-    $.each(dataset['variables'], function(_index, variable) {
-      $('#results-list').append(createResultListItem(dataset, variable, size, source,
-        timeRange));
-    });
-  });
-
-//  var tr = d3.select('#results-list').selectAll("li")
-//    .data(results, function(d) {
-//      return d['id'];
-//    });
-//
-//  var rows = tr.enter().append('li');
-//
-//  $.each(tr.exit()[0], function(index, row) {
-//    if (row) {
-//      selection = d3.select(row);
-//      if (removeFilter(selection.data()[0]))
-//        selection.remove();
-//    }
-//  });
-//
-//  var content = rows.selectAll('div')
-//    .data(function(row) {
-//      // Display the tags, we should probably truncate the list ...
-//      var tags = [];
-//      $.each(row['variables'], function(index, variable) {
-//        tags = tags.concat(variable['tags']);
-//      });
-//
-//      var size = row['size'];
-//
-//      if (size && size != 'N/A') {
-//        size = Math.round(row['size']/1024/1024) + "M"
-//      }
-//
-//      return [ {column: 'name', data: row['name']},
-//               {column: 'source', data: row['source']},
-//               {column: 'size', data: size},
-//               {column: 'tags', data: tags.join()}] ;
-//    });
-//
-//  content = content.enter().append('div');
-//  content.text(function(d) { return d['data']; });
-//  content.each(function(d, i) { $(this).addClass(d['column']);});
-//
-//  // Populate the timesteps parameter list
-//  var selectTimestep = rows.append('td');
-//  selectTimestep.classed('timesteps', true);
-//  selectTimestep = selectTimestep.append('select');
-//  selectTimestep.classed('timestep-select', true);
-//
-//  selectTimestep = selectTimestep.selectAll('select').data(function(row) {
-//
-//    var timestep  = ['N/A'];
-//
-//    if (row && 'timeInfo' in row && row['timeInfo'].rawTimes)
-//      timestep = row['timeInfo'].rawTimes;
-//
-//    return timestep;
-//  });
-//
-//  selectTimestep.enter().append('option').text(function(timestep) {
-//    return timestep;
-//  });
-//  selectTimestep.exit().remove();
-//
-//  // Populate the parameter list
-//  var select = rows.append('td');
-//  select.classed('parameter', true);
-//  select = select.append('select');
-//  select.classed("parameter-select", true);
-//
-//  select = select.selectAll('select').data(function(row) {
-//    var variables = [];
-//    $.each(row['variables'], function(index, variable) {
-//      variables = variables.concat(variable['name']);
-//    });
-//
-//    return variables;
-//  });
-//
-//  select.enter().append('option').text(function(variable) {
-//    return variable;
-//  });
-//  select.exit().remove();
-
-  $('#results-list div').draggable( {
-    cursor: 'move',
-    containment: 'window',
-    appendTo: 'body',
-    helper: function(event) {
-      var $this = $(this),
-        dataset = $this.data('dataset'),
-        variable = $this.data('variable'),
-        timestep = null,
-        drag;
-
-    if (dataset && 'timeInfo' in dataset && 'rawTimes' in dataset['timeInfo']) {
-      timestep = dataset['timeInfo'].rawTimes[0];
-    }
-
-    drag = $('<div id="parameter" class="whatadrag">' + variable['name'] + '</div>');
-
-    drag.data("dataset", {
-      name: dataset.name,
-      dataset_id: dataset.id,
-      source: dataset.source,
-      parameter: variable['name'],
-      timestep: timestep,
-      timeInfo: dataset.timeInfo,
-      url: dataset.url,
-      size: dataset.size,
-      checksum: dataset.checksum,
-      basename: dataset.basename
-    });
-
-    return drag;
-    }
-  })
-};
-
-//////////////////////////////////////////////////////////////////////////////
-/**
- * Main program
- *
- */
-//////////////////////////////////////////////////////////////////////////////
-archive.main = function() {
-  init();
-
-  $('#error-dialog').hide();
-  archive.initQueryInterface();
 
   var mapOptions = {
     zoom : 6,
@@ -620,38 +292,8 @@ archive.main = function() {
     $('#collapse-documents').find('.input-small').tooltip();
   });
 
-  /* set up workflow editor */
-  archive.workflowEditor = ogs.wfl.editor({div: "workflowEditor"});
-
-  $('#workflow-dialog').resizable().draggable({ handle: ".modal-header" });
-
-  $('#workflow-dialog').on("resize", function(event, ui) {
-    var footerHeight = $('#workflow-dialog .modal-footer').outerHeight(),
-      headerHeight = $('#workflow-dialog .modal-header').outerHeight(),
-      $body = $('#workflow-dialog .modal-body'),
-      paddingTop = parseInt($body.css('padding-top'), 10),
-      paddingBottom = parseInt($body.css('padding-bottom'), 10),
-      height = ui.size.height - headerHeight - footerHeight - paddingTop - paddingBottom;
-
-    $(ui.element).find(".modal-body").each(function() {
-      $(this).css("max-height", height);
-    });
-
-    $('#workflowEditor').css('height', height);
-
-    archive.workflowEditor.resize();
-  });
-
-  $('#workflow-dialog .modal-body').css('margin-bottom', 0);
   $('.ui-resizeable-s').css('bottom', 0);
   $('.ui-resizeable-e').css('right', 0);
-
-  // Populate the algorithm list
-  for(var name in staticWorkflows) {
-    if(staticWorkflows.hasOwnProperty(name)) {
-      $('#algorithm-select').append($('<option>'+name+'</option>'));
-    }
-  }
 
   archive.userName(function(openIdUri) {
     var parts = openIdUri.split('/');
@@ -671,32 +313,6 @@ archive.main = function() {
   });
 
 };
-
-//////////////////////////////////////////////////////////////////////////////
-/**
- * Initialize web sockets
- */
-//////////////////////////////////////////////////////////////////////////////
-archive.initWebSockets = function() {
-  var ws = srvModule.webSocket({nodes: ['streammaster','streamworker']}),
-    streaming = false;
-
-  ws.bind('streammaster', function(message) {
-    if(message == 'done') {
-      streaming = false;
-      //$('#button').html(startText);
-    } else {
-      var img = new Image();
-      img.onload = function() {
-        ctx.drawImage(img, parseInt(message.x), parseInt(message.y)-img.height);
-      };
-      img.src = "data:image/png;base64," + message.img;
-      recieveCount += 1;
-    }
-  });
-};
-
-
 
 //////////////////////////////////////////////////////////////////////////////
 /**
